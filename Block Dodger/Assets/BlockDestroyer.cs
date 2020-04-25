ba9c0dda3c693dbb4e731789171451302a46d4dd
itZSaint099 <soumyajithalder099@gmail.com>
2020-04-25 18:30:51 +0530

    Initial Commit
    Added Welcom UI Screen, End Screen with Restart Game and Quit Button

---
+++ Block Dodger/Assets/BlockDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDestroyer : MonoBehaviour
+{
+    public float gravityScaleIncrements = 20f;
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / gravityScaleIncrements;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -2f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
