83787d275246fbe4ca73d3ee0100feffd6ae7c6d
itZSaint099 <soumyajithalder099@gmail.com>
2020-06-30 14:52:16 +0530

    Initial Commit: Completed The Game  - Added player movement, ball, chain, line collision scripts  - Added Prefabs  - Added Objects  - Reorganized the files in their respective folders  - TODO: - Add Start, Restart, Game Over scene.                 - Add effects or slow motion or anything that applies                 - Test out the game for bugs

---
+++ Bubble Trouble/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 startForce;
+
+    public Rigidbody2D rb;
+    public GameObject spawnBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(startForce, ForceMode2D.Impulse);
+    }
+
+    public void Split()
+    {
+        if(spawnBall != null)
+        {
+            GameObject ball_1 = Instantiate(spawnBall, rb.position + Vector2.right / 4f, Quaternion.identity);
+            GameObject ball_2 = Instantiate(spawnBall, rb.position + Vector2.left / 4f, Quaternion.identity);
+
+            ball_1.GetComponent<Ball>().startForce = new Vector2(2f, 4f);
+            ball_2.GetComponent<Ball>().startForce = new Vector2(-2f, 4f);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Bubble Trouble/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 4f;
+
+    private float movement = 0f;
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector2(movement * Time.fixedDeltaTime, 0f));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ball")
+        {
+            // GAME OVER
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
