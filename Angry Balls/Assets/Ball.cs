8785af2395688b4ada442751f0c899a64d9b3988
itZSaint099 <soumyajithalder099@gmail.com>
2020-08-01 19:47:27 +0530

    Initial Commit: Added Sprites, Made a simple level, Added Ball Script

---
+++ Angry Balls/Assets/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float ReleaseDelay = .15f;
+
+    private bool isPressed = false;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release ()
+    {
+        yield return new WaitForSeconds(ReleaseDelay);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+}
