9b61923a20e1c95897903d6d39b6933c6dc97d1d
itZSaint099 <soumyajithalder099@gmail.com>
2020-08-08 19:21:20 +0530

    Added Ball and Enemy logic Added Collision Detection Added Enemy lives and Ball numbers Added Next Level scene management
    Left:
     - Add new new scripts logic
     - Add new levels

---
+++ Angry Balls/Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+    public GameObject nextBall;
+
+    public float releaseDelay = .15f;
+    public float maxPullDist = 3f;
+
+    private bool isPressed = false;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector3.Distance(mousePos, hook.position) > maxPullDist)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxPullDist;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseDelay);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if(nextBall != null)
+        {
+            nextBall.SetActive(true);
+        }
+        else
+        {
+            Enemy.EnemiesAlive = 0;
+            Debug.Log("Next Level");
+            //Next level
+        }
+    }
+}
---
+++ Angry Balls/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject deathEffect;
+
+    public float health = 4f;
+
+    public static int EnemiesAlive = 0;
+
+    void Start()
+    {
+        EnemiesAlive++;
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        float relVelocity = collisionInfo.relativeVelocity.magnitude;
+
+        if (relVelocity > health)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        
+        EnemiesAlive--;
+        if(EnemiesAlive<= 0)
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("Restart");
+        }
+
+        Destroy(gameObject);
+    }
+}
